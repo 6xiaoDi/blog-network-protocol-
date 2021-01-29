4ec0eadedef96b7b5b5e7628472b994983a8a1b1
6xd <ldtheended@163.com>
2021-01-29 15:13:34 +0800

    a0.06（CORS跨域请求限制-解决自定义头不被允许）

--- cors/server2.js
+++ cors/server2.js
@@ -4,7 +4,8 @@ http.createServer(function (request, response) {
   console.log('request come', request.url)
 
   response.writeHead(200, {
-    'Access-Control-Allow-Origin': '*'
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'X-Test-Cors',
   })
   response.end('123')
 }).listen(8887)
