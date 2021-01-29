2fc139aad56fa5c716addc96269e817de5cb338d
6xd <ldtheended@163.com>
2021-01-29 14:17:56 +0800

    a0.04（CORS跨域请求限制-安全方式）

--- cors/server2.js
+++ cors/server2.js
@@ -4,7 +4,7 @@ http.createServer(function (request, response) {
   console.log('request come', request.url)
 
   response.writeHead(200, {
-    'Access-Control-Allow-Origin': '*'
+    'Access-Control-Allow-Origin': 'http://127.0.0.1:8888',
   })
   response.end('123')
 }).listen(8887)
