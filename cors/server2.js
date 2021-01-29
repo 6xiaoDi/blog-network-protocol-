71cbcefdb36bea2dc94b9b32c7dc332668c50110
6xd <ldtheended@163.com>
2021-01-29 13:30:21 +0800

    a0.02（CORS跨域请求限制-报错）

---
+++ cors/server2.js
@@ -0,0 +1,9 @@
+const http = require('http')
+
+http.createServer(function (request, response) {
+  console.log('request come', request.url)
+
+  response.end('123')
+}).listen(8887)
+
+console.log('server listening on 8887')
