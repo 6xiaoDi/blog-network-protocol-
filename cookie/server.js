afee689183fe8757b31a04b1a426b477d933e204
6xd <ldtheended@163.com>
2021-01-30 10:36:32 +0800

    a0.10（Cookie——基本使用）

---
+++ cookie/server.js
@@ -0,0 +1,20 @@
+const http = require('http')
+const fs = require('fs')
+
+http.createServer(function (request, response) {
+  console.log('request come', request.url)
+
+  if (request.url === '/') {
+    const html = fs.readFileSync('test.html', 'utf8')
+    response.writeHead(200, {
+      'Content-Type': 'text/html',
+      // 'Set-Cookie': 'id=123'
+      // 'Set-Cookie': ['id=123;max-age=2', 'abc=456']
+      'Set-Cookie': ['id=123;max-age=2', 'abc=456;HttpOnly']
+    })
+    response.end(html)
+  }
+
+}).listen(8888)
+
+console.log('server listening on 8888')
